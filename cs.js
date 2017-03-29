bc8d037d1e502ad0218121bb62fa0fdfad2c5c5d
lyndonl3091 <lyndonl3091@gmail.com>
2017-03-29 19:32:30 -0400

    delete doubly

--- cs.js
+++ cs.js
@@ -173,3 +173,34 @@ doublyLinkedList.prototype.push = function(val) {
    }
   return list;
 }
+
+// delete node from double linked list
+
+function deleteNodeDLL(val){
+   var current = dll.head,
+       previous;
+
+   //delete head
+   if(current.value == val){
+      dll.head = current.next;
+      //if there is only one node, then dll.head is null
+      if(dll.head) dll.head.previous = null;
+      return dll;
+   }
+
+   while(current.next){
+      if(current.value == val){
+         previous.next = current.next;
+         current.next.previous = previous;
+         return dll;
+     }
+     previous = current;
+     current = current.next;
+   }
+
+   //delete last node
+   if(current.value == val){
+     previous.next = null;
+   }
+   return dll;
+}
